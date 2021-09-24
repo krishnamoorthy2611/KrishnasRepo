d3a09fcbe654aa317cd1149a5265ed5b1f52cb42
krishnamoorthy2611 <krishnamoorthy2611@yahoo.co.in>
2021-09-24 17:18:53 +0530

    added few changes in testsample file

--- seleniumautomation/src/main/java/BaseClass/TestSample.java
+++ seleniumautomation/src/main/java/BaseClass/TestSample.java
@@ -16,7 +16,9 @@ public class TestSample {
 		// TODO Auto-generated method stub
 		
 		System.out.println("hello world");
-
+//added another code snippet
+		System.out.println("this is sample snippet");
+		
 	}
 
 }
