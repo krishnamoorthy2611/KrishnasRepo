df375e635b1fa4c7cd133c9266df526dc3a64822
krishnamoorthy2611 <krishnamoorthy2611@yahoo.co.in>
2021-09-24 17:16:11 +0530

    added test sample class

---
+++ seleniumautomation/src/main/java/BaseClass/TestSample.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package BaseClass;
+
+/**
+ * @author Krishnamoorthy
+ *
+ */
+public class TestSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+
+	}
+
+}
