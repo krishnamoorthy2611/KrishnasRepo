d88c6fbe5fb005cbe7304bc901a84ec3c4b40477
krishnamoorthy2611 <krishnamoorthy2611@yahoo.co.in>
2021-09-24 17:50:06 +0530

    added first project

---
+++ SeleniumAutomationGit/src/Baseclass/Testsamplefile.java
@@ -0,0 +1,15 @@
+package Baseclass;
+
+public class Testsamplefile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+		
+	}
+
+}
